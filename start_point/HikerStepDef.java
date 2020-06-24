0abdfe2b139acf33953d7a18999244207e43fb37
Seb Rose <seb@claysnow.co.uk>
2020-06-24 10:27:59 +0100

    Updated to Spring 5.2.7

--- start_point/HikerStepDef.java
+++ start_point/HikerStepDef.java
@@ -1,7 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import cucumber.api.java.en.*;
+import io.cucumber.java.en.*;
 
 public class HikerStepDef {
 
@@ -10,16 +10,16 @@ public class HikerStepDef {
 
     private int answer;
 
-    @Given("^the hitch-hiker selects some tiles$")
+    @Given("the hitch-hiker selects some tiles")
     public void theHitchHikerSelectsSomeTiles() throws Throwable {
     }
 
-    @When("^they spell (\\d+) times (\\d+)$")
+    @When("they spell {int} times {int}")
     public void theySpellTimes(int arg1, int arg2) throws Throwable {
-        answer = hiker.answer(arg1,arg2);
+        answer = Hiker.answer(arg1,arg2);
     }
 
-    @Then("^the score is (\\d+)$")
+    @Then("the score is {int}")
     public void theScoreIs(int expected) throws Throwable {
         assertEquals(expected, answer);
     }
--- start_point/RunCukesTest.java
+++ start_point/RunCukesTest.java
@@ -1,17 +1,15 @@
-import cucumber.api.junit.Cucumber;
-import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    strict = true,
-    monochrome = true,
-    plugin = { "pretty", "junit:/tmp/output" },
-    snippets = SnippetType.CAMELCASE
+monochrome = true,
+    plugin = {"pretty"},
+    strict = true
 )
 public class RunCukesTest {
-    // this is the adapter/bridge code
-    // between cucumber jvm and junit
-    // you do not need to edit this
+  // this is the adapter/bridge code
+  // between cucumber jvm and junit
+  // you do not need to edit this
 }
